76598ed1ade2c71621f73b54d48c90c8c11dfe43
KIMINJOONG <ijboym216@gmail.com>
2020-02-25 14:02:38 +0900

    Updates

---
+++ precache-manifest.19eccbb1cdf7b24e693f759b868e3b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b2e3c946442b2bb858bc0a15067aad",
+    "url": "/deploy_test/index.html"
+  },
+  {
+    "revision": "620f06661c5db4c723ba",
+    "url": "/deploy_test/static/css/2.33a25a1d.chunk.css"
+  },
+  {
+    "revision": "620f06661c5db4c723ba",
+    "url": "/deploy_test/static/js/2.35b01d30.chunk.js"
+  },
+  {
+    "revision": "ee638b47465949dfa72f2e4265f61a63",
+    "url": "/deploy_test/static/js/2.35b01d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362d00ed3585be73fe5d",
+    "url": "/deploy_test/static/js/main.bd20e401.chunk.js"
+  },
+  {
+    "revision": "62953f8aaeb0a62be263",
+    "url": "/deploy_test/static/js/runtime-main.a5738b3b.js"
+  }
+]);
